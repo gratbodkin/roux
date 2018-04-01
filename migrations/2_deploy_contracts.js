cdc7ff0060b8fc871f529887dbea6aff92a5245c
Grattan Bodkin <gbodkin@uaudio.com>
2018-04-01 14:38:04 -0700

    clean up test migration code

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,9 +8,9 @@ var ROUX_Token = artifacts.require("ROUX_Token");
 var SplitPayment = artifacts.require("SplitPayment");
 
 //TEMPORARY TEST ACCOUNTS --> NOT FOR DEPLOYMENT ON MAIN NET!!
-//const ownerAcct = "0xc743efc8b3ce4bd9a034c8aea837bffa228f54a3";
+
 const ownerAcct = "0x63cF88292f39CF4a5871b0733f226f12728A13eD";
-const account1 = "0x63cF88292f39CF4a5871b0733f226f12728A13eD";
+const account1 = "0xa8B3F268406FF62621e3C911E39a174232DdD8E2";
 const account2 = "0x26697ebd1363EF44D4a61dBa334E13B0cef1eF57";
 const account3 = "0xe4D194f0E5Fd679e8E0388bA7CbE4dc6e2941a8E";
 
@@ -26,56 +26,35 @@ module.exports = function (deployer, network) {
   const minimumPurchase = web3.toWei("1", "ether");
   const initialRate = 9166;
   const finalRate = 5500;
-  return deployer.deploy(ROUX_Token, {from: ownerAcct}).then(() => {
-       return ROUX_Token.deployed().then(async ROUX_TokenInstance => {
-         return deployer.deploy(
-         ROUX_ReferralBasedCrowdsale,
-         start,
-         finish,
-         initialRate,
-         finalRate,
-         cap,
-         splitPaymentAddr,
-         ROUX_TokenInstance.address,
-         {from: ownerAcct})
-         .then(() => {
-         return ROUX_ReferralBasedCrowdsale.deployed().then(async ROUX_ReferralBasedCrowdsaleInstance => {
-           await ROUX_TokenInstance.transferOwnership(ROUX_ReferralBasedCrowdsaleInstance.address, {from: ownerAcct});
+   return deployer.deploy(
+   SplitPayment,
+   [account2,account3,account1],
+   [150,150,700],
+   {from: ownerAcct})
+   .then(() => {
+       return SplitPayment.deployed()
+         .then(async SplitPaymentInstance => {
+           return deployer.deploy(ROUX_Token, {from: ownerAcct}).then(() => {
+                return ROUX_Token.deployed().then(async ROUX_TokenInstance => {
+                  return deployer.deploy(
+                  ROUX_ReferralBasedCrowdsale,
+                  start,
+                  finish,
+                  initialRate,
+                  finalRate,
+                  cap,
+                  SplitPaymentInstance.address,
+                  ROUX_TokenInstance.address,
+                  {from: ownerAcct})
+                  .then(() => {
+                  return ROUX_ReferralBasedCrowdsale.deployed().then(async ROUX_ReferralBasedCrowdsaleInstance => {
+                    await ROUX_TokenInstance.transferOwnership(ROUX_ReferralBasedCrowdsaleInstance.address, {from: ownerAcct});
+                  });
+               });
+              });
+            });
          });
-      });
-     });
-   });
-  // return deployer.deploy(
-  // SplitPayment,
-  // [account2,account3],
-  // [150,150],
-  // {from: ownerAcct})
-  // .then(() => {
-  //     return SplitPayment.deployed()
-  //       .then(async SplitPaymentInstance => {
-  //           return deployer.deploy(ROUX_Token, {from: ownerAcct})
-  //             .then(() => {
-  //                 return ROUX_Token.deployed().then(async ROUX_TokenInstance => {
-  //                     return deployer.deploy(
-  //                     ROUX_ReferralBasedCrowdsale,
-  //                     start,
-  //                     finish,
-  //                     initialRate,
-  //                     finalRate,
-  //                     cap,
-  //                     SplitPaymentInstance.address,
-  //                     ROUX_TokenInstance.address,
-  //                     minimumPurchase,
-  //                     {from: ownerAcct,gas: 6712390})
-  //                     .then(() => {
-  //                     return ROUX_ReferralBasedCrowdsale.deployed().then(async ROUX_ReferralBasedCrowdsaleInstance => {
-  //                       await ROUX_TokenInstance.transferOwnership(ROUX_ReferralBasedCrowdsaleInstance.address);
-  //                     });
-  //                 });
-  //             });
-  //         });
-  //     });
-  // });
+    });
 };
 
 function latestTime() {
